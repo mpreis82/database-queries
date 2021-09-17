fe776296dc388fed5cd341e182b96b6cba69980d
mpreis82 <mpreis82@gmail.com>
2021-09-17 16:35:29 -0300

    Repository implementation

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -15,16 +15,23 @@ export class GamesRepository implements IGamesRepository {
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
       .createQueryBuilder()
-      // Complete usando query builder
+      .where("title ILIKE :title", { title: `%${param}%` })
+      .getMany();
+    // Complete usando query builder
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("select count(*) as count from games"); // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
+    const game = await this.repository.findOne(id);
+
+    return await this.repository
       .createQueryBuilder()
-      // Complete usando query builder
+      .relation(Game, 'users')
+      .of(game)
+      .loadMany();
+    // Complete usando query builder
   }
 }
--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,25 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository.findOne(user_id, { relations: ['games'] });
+
+    if (!user) {
+      throw new Error("User not exists");
+    }
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("select * from users order by first_name asc"); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(
+      "select * from users where lower(first_name) = $1 and lower(last_name) = $2",
+      [first_name.toLowerCase(), last_name.toLowerCase()]
+    ); // Complete usando raw query
   }
 }
